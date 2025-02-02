ec66a1395b90ecf5e77e7457924d27fd3c3c0509
Ömer Faruk Sezen <104766543+mexikali@users.noreply.github.com>
2025-02-02 23:10:32 +0300

    is_admin is added to the user table

--- ticket-reservation-api/database/migrations/0001_01_01_000000_create_users_table.php
+++ ticket-reservation-api/database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('tel_no')->unique();
+            $table->boolean('is_admin')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
